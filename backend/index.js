343238c91c93543815404ae3b6306e979c8776ba
Greg Rogers <gregrogers@Gregs-MacBook-Air.local>
2017-02-16 11:04:18 -0500

    Basic index set up

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+var http = require("http")
+var express = require("express")
+var app = express()
+app.use(express.static(__dirname + "/"));
+var port = process.env.PORT || 5000;
+var server = http.createServer(app);
+server.listen(port);
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/../frontend/index.html'));
+});
+
