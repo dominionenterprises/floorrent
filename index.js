07c784149e7c800c44c72661426cc77a4fd54302
Greg Rogers <gregrogers@Gregs-MacBook-Air.local>
2017-02-16 18:19:13 -0500

    Fixed CORS

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+app.use(allowCrossDomain);
+
+
 // serve static files from public
 app.use(express.static('public'));
 
