94b9ebac1c7451551ae9c7168f2f0fadffffbf06
Greg Rogers <gregrogers@Gregs-MacBook-Air.local>
2017-02-16 11:55:14 -0500

    Restructure

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
-var http = require("http")
-var express = require("express")
-var app = express()
+var http = require("http");
+var express = require("express");
+var app = express();
 app.use(express.static(__dirname + "/"));
 var port = process.env.PORT || 5000;
 var server = http.createServer(app);
 server.listen(port);
 var path = require('path');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../frontend/index.html'));
 });
 
+app.post("/login", function(req, res){
+  username = req.body.username;
+  password = req.body.password;
+});
