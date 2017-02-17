3433b6e6187222925b7e8d4f395bea28607343b9
Greg Rogers <gregrogers@Gregs-MacBook-Air.local>
2017-02-17 03:28:56 -0500

    New name text field, isAdmin bool

--- public/client.js
+++ public/client.js
@@ -1,4 +1,5 @@
 var apihost = "https://brainstorm-backend.herokuapp.com";
+var isAdmin = false;
 
 function closeLoginBox(){
   var loginBox = document.getElementById("login");
@@ -20,12 +21,17 @@ function openSelectionBox(){
 }
 
 function closeSelectionBox(){
-  var mainContainer = document.getElementById("main-container");
   var selectionBox = document.getElementById("selection-box");
+  setTimeout(function(){
+    selectionBox.style.display = "none";
+  }, 250);
+}
+
+function resetMainContainer(){
+  var mainContainer = document.getElementById("main-container");
   var loginSheet = document.getElementById("login-sheet");
   mainContainer.style.filter = "blur(0px) saturate(1)";
   setTimeout(function(){
-    selectionBox.style.display = "none";
     loginSheet.style.display = "none";
   }, 250);
 }
@@ -71,10 +77,12 @@ function loginButtonClick(e){
     username: username,
     password: password
   }, function(data){
-    if (data.status == 200)
+    if (data.status == 200){
+      isAdmin = true;
       closeLoginBox();
-    else
+    } else {
       makeLoginRed();
+    }
   });
 }
 
@@ -152,8 +160,6 @@ function loadAvailableFloorplans(){
   pane.appendChild(border);
   floorplanScroll.appendChild(pane);
   pane.addEventListener("click", handleNewFloorplanPaneClick);
-  
-  //TODO: Add 'create new' button
 }
 
 function handleFloorplanPaneClick(e){
@@ -166,7 +172,20 @@ function handleFloorplanPaneClick(e){
 
 function handleNewFloorplanPaneClick(e){
   console.log("NEW FLOORPLAN");
-  closeSelectionBox();
+  //TODO: Bring up a name box
+  setTimeout(openNameBox, 250);
+}
+
+function openNameBox(){
+  var nameBox = document.getElementById("newname-box");
+  nameBox.style.display = "inherit";
+  setTimeout(function(){
+    nameBox.style.opacity = "1";
+  }, 10);
+}
+
+function closeNameBox(){
+  resetMainContainer();
 }
 
 function pageRefresh(){
